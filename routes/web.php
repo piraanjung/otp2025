15baa674d8d0f20f9d10066fed37666fa352e25c
Pipatpong H <pipatpongh@Pipatpongs-Mac-mini.local>
2023-10-30 14:24:46 +0700

    web.php add inv controller resource

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->grou
     Route::delete('/users/{user}/roles/{role}', [UserController::class, 'removeRole'])->name('users.roles.remove');
     Route::post('/users/{user}/permissions', [UserController::class, 'givePermission'])->name('users.permissions');
     Route::delete('/users/{user}/permissions/{permission}', [UserController::class, 'revokePermission'])->name('users.permissions.revoke');
-
 });
+
 Route::middleware(['auth', 'role:admin, finance'])->prefix('invoice')->group(function(){
     Route::resource('/invoice', InvoiceController::class);
 });
